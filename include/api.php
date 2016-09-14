3fd2268fd1b867a43e2b706d81510c378d305985
裴伟伟 <peiweiwei@wecash.net>
2016-09-14 10:28:50 +0800

    fix: fix id card query bug

--- include/api.php
+++ include/api.php
@@ -42,7 +42,7 @@ class API
             return $this->type_ary[0];
         }elseif (preg_match("/^1[34578]\d{9}$/", $account)){	// Verify mobile type
             return $this->type_ary[1];
-        }elseif (preg_match("/^\d{5,}$/", $account)) {      // Verify qq number type
+        }elseif (preg_match("/^\d{5,15}$/", $account)) {      // Verify qq number type
             return $this->type_ary[2];
         }elseif (preg_match("/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4}$/", $account) 
             || preg_match("/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/", $account)) {   // Verify id card type
